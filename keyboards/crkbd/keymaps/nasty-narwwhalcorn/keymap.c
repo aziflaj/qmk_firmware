f45afcea13291c67b0655cd1cf78e2f7d4fe8909
Aldo Ziflaj <aldo.ziflaj@leadfeeder.com>
2023-08-08 11:47:16 +0200

    always reachable home row mods

--- keyboards/crkbd/keymaps/nasty-narwwhalcorn/keymap.c
+++ keyboards/crkbd/keymaps/nasty-narwwhalcorn/keymap.c
@@ -39,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-    XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX,  _______,    _______,   _______,   _______,   XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
     XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                        _______, _______,   KC_SPC,        KC_ENT,  _______, _______
+                                        _______, _______,   KC_SPC,        KC_ENT, _______,  KC_DEL
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_EXLM,  KC_AT,  KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-    XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX,  _______,    _______,   _______,   _______,   XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, XXXXXXX,
     XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                         _______, _______,   KC_SPC,        KC_ENT,  _______, _______
   ),
